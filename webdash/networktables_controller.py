07b647db8ea52b8c67e889a34f7445575fee0b7b
Christian J. Balcom <robot.inventor@gmail.com>
2015-05-05 12:29:54 -0400

    Fixed #1 -- array editing works now.

--- webdash/networktables_controller.py
+++ webdash/networktables_controller.py
@@ -1,4 +1,5 @@
 from networktables import NetworkTable
+from networktables2.type import ArrayData, BooleanArray, NumberArray, StringArray
 import asyncio
 import json
 import math
@@ -25,8 +26,7 @@ class ConnectionListener:
         set_local_value("~CONNECTED~", False)
 
 def val_listener(key, value, isNew):
-    #print("val_listener: key={}, value={}, type={}".format(key, value, type(value)))
-    set_local_value(key, value)
+    set_local_value(key, value, force_type=True)
 
 def get_local_value(key):
     with table_data_lock:
@@ -34,12 +34,18 @@ def get_local_value(key):
             key = key[1:]
         value = table_data
         for s in key.split(NetworkTable.PATH_SEPARATOR):
-            if s not in value:
-                return None
-            value = value[s]
+            if isinstance(value, list):
+                s = int(s)
+                if len(value) <= s:
+                    return None
+                value = value[s]
+            else:
+                if s not in value:
+                    return None
+                value = value[s]
         return value
 
-def set_local_value(key, value):
+def set_local_value(key, value, force_type=False):
     with table_data_lock:
         if isinstance(value, float) and (math.isnan(value) or math.isinf(value)):
             value = 0.0
@@ -59,10 +65,14 @@ def set_local_value(key, value):
         if value_key == "":
             return
 
-        if value_key in target_table:
+        if value_key in target_table and not force_type:
             value = type(target_table[value_key])(value)
-
-        target_table[value_key] = value
+        if isinstance(target_table, dict):
+            target_table[value_key] = value
+        elif isinstance(target_table, list):
+            target_table[int(value_key)] = value
+        else:
+            raise ValueError("Unknown Table Type {}".format(type(target_table)))
         trigger_update()
 
 def trigger_update():
@@ -77,12 +87,34 @@ def set_value(key, value):
         if key[0] == NetworkTable.PATH_SEPARATOR:
             key = key[1:]
 
-        if isinstance(value, bool):
-            root_table.putBoolean(key, value)
-        elif isinstance(value, float) or isinstance(value, int):
-            root_table.putNumber(key, value)
+        # First check if value is part of a list
+        parent_object = None
+        if NetworkTable.PATH_SEPARATOR in key:
+            final_sep_idx = key.rfind(NetworkTable.PATH_SEPARATOR)
+            parent_path = key[:final_sep_idx]
+            key_end = key[final_sep_idx+1:]
+            parent_object = root_table.getValue(parent_path, None)
+
+        if isinstance(parent_object, list):
+            parent_object[int(key_end)] = value
+
+            if isinstance(value, bool):
+                new_arraydata = BooleanArray()
+            elif isinstance(value, float) or isinstance(value, int):
+                new_arraydata = NumberArray()
+            else:
+                new_arraydata = StringArray()
+
+            new_arraydata.extend(parent_object)
+
+            root_table.putValue(parent_path, new_arraydata)
         else:
-            root_table.putString(key, str(value))
+            if isinstance(value, bool):
+                root_table.putBoolean(key, value)
+            elif isinstance(value, float) or isinstance(value, int):
+                root_table.putNumber(key, value)
+            else:
+                root_table.putString(key, str(value))
     except Exception as e:
         print(e)
     finally:
@@ -182,6 +214,7 @@ def networktables_websocket_listener(ws):
     while True:
         try:
             jdata = yield from ws.receive_str()
+            #print("Recieved " + jdata)
         except Exception:
             return
         data = json.loads(jdata)
